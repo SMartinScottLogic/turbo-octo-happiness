6d8889509a5521d98d1b76874c5b4c516e422f81
Simon Martin <smartin@scottlogic.com>
2020-05-15 07:15:10 +0000

    Logger setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use chrono::Local;
+
+#[macro_use]
+extern crate log;
+
+struct ConsoleLogger;
+
+impl log::Log for ConsoleLogger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        println!(
+            "{} {} {} - {}",
+            Local::now().format("%Y-%m-%dT%H:%M:%S%z"),
+            record.level(),
+            record.target(),
+            record.args()
+        );
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: ConsoleLogger = ConsoleLogger;
+
 fn main() {
-    println!("Hello, world!");
+    log::set_logger(&LOGGER).unwrap();
+    log::set_max_level(log::LevelFilter::Debug);
+
+    info!("Starting organiseFS...");
 }
